0c632bd8a7b5e804ea0f48a36bd3bdb167468b61
Salario Mínimo <102463669+SalarioMinimo@users.noreply.github.com>
2022-04-09 19:29:20 -0600

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -3,8 +3,15 @@ import nltk
 from nltk.tokenize import word_tokenize
 nltk.download("punkt")
 
+
+fuctions = {más:"+"}
+equation = ""
+
 input = "dos más dos"
 
 input = word_tokenize(input)
 
-input
+for x in input:
+  equation += x
+
+traslator()
