c9f5b95695f9af42e060a34a767c9b5080118c8d
Salario Mínimo <102463669+SalarioMinimo@users.noreply.github.com>
2022-04-10 21:50:42 -0600

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -2,16 +2,8 @@ import streamlit as st
 import nltk
 from nltk.tokenize import word_tokenize
 nltk.download("punkt")
-  
-class calculator:
-  
-  def __init__(self):
-    self.text = "2 más 2"
-    self.text = word_tokenize(self.text)
-    capitalismo()
-  
-  def capitalismo(self):
-    st.text(self.text)
-    st.title("it works")
-    
-uwu=calculator
+
+texto=""
+st.text_area(texto,label="")
+texto
+
