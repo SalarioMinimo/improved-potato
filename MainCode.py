f30dc58db2b1590e705ff397ecb36d168a2292d9
Salario Mínimo <102463669+SalarioMinimo@users.noreply.github.com>
2022-04-11 18:43:56 -0600

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -6,11 +6,21 @@ nltk.download("punkt")
 class calculator:
   
   def __init__(self,input):
+    self.send = ""
     self.text=word_tokenize(input)
+    for x in self.text:
+      if self.text[x] in self.functions:
+        self.functions[self.text[x]](x)
+    
+  def más(self,index):
+    self.send = self.text[x-1]+self.text[x+1]
+    
   
-  def value(self):
-    return "uwu"
- 
+  def __str__(self):
+    return "self.send"
+  
+  self.functions = {}
+  self
 
 
 texto=st.text_area(label="muerte al capital")
