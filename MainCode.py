c00a45ee1d9896be765d67ca2be0012156efc899
Salario Mínimo <102463669+SalarioMinimo@users.noreply.github.com>
2022-04-13 01:38:29 -0600

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -14,12 +14,13 @@ class calculator:
     self.send = ""
     self.text=word_tokenize(input)
     self.functions = {"más":self.mas,"menos":self.menos,"por":self.por,"entre":self.entre}
-    
-    
-    for x in range(len(self.text)):
-      
-      if self.text[x] in self.functions:
-        self.functions[self.text[x]](x)
+    c=1
+    while c != 0:
+      c = 0
+      for x in range(len(self.text)):
+        if self.text[x] in self.functions:
+          self.functions[self.text[x]](x)
+          c += 1
     
   def mas(self,index):
     self.text[index]="+"
