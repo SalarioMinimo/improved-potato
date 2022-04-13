ecbf9945a7ee2eaa9edfacfb0909f276af32a9bc
Salario Mínimo <102463669+SalarioMinimo@users.noreply.github.com>
2022-04-13 16:07:13 -0600

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -52,7 +52,7 @@ resultado = symbols("resultado")
 texto=st.text_area(label="muerte al capital")
 tremendo = str(calculator(texto))
 
-result = solve(tremendo + (" -resultado"), resultado,Check=False)
+result = solve(tremendo + (" -resultado"), resultado)
 
 
 tremendo
