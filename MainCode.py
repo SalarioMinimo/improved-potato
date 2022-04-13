1e5734f8939a8544300c248c90c5358c6c8a6b02
Salario Mínimo <102463669+SalarioMinimo@users.noreply.github.com>
2022-04-12 23:45:07 -0600

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -1,20 +1,29 @@
 import streamlit as st
 import nltk
 from nltk.tokenize import word_tokenize
+import math
+import sympy
 nltk.download("punkt")
 
 class calculator:
   
   def __init__(self,input):
+    
+    
     self.send = ""
     self.text=word_tokenize(input)
     self.functions = {"más":self.mas}
+    
+    
     for x in range(len(self.text)):
+      
       if self.text[x] in self.functions:
         self.functions[self.text[x]](x)
     
+    
+    
   def mas(self,index):
-    self.send = self.text[index-1]+self.text[index+1]
+    self.text[index]="+"
     
   
   def __str__(self):
