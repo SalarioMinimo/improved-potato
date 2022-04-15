6d339ea46acf3c453cb0a6bd213ba740e17ab45e
Salario Mínimo <102463669+SalarioMinimo@users.noreply.github.com>
2022-04-15 17:49:18 -0600

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -50,7 +50,7 @@ class calculator:
 
 resultado = symbols("resultado")
 texto=st.text_area(label="muerte al capital")
-tremendo = sympify(calculator(texto),evalueate=False)
+tremendo = sympify(calculator(texto),evaluate=False)
 
 
 tremendo
