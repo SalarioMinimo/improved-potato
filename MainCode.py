3a64f855830b5ebf8604bbd9743da157d0bdfeaa
Salario Mínimo <102463669+SalarioMinimo@users.noreply.github.com>
2022-04-19 02:09:45 -0600

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -96,7 +96,7 @@ class calculator:
 resultado = symbols("resultado")
 texto=st.text_area(label="muerte al capital")
 prueba = word_tokenize(texto)
-annotated_text(exec('("no","mames")'))
+annotated_text(eval('("no","mames")'))
 st.text(calculator(texto))
 tremendo = sympify(calculator(texto),evaluate=False)
 
