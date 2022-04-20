abea84152a84d85747fc15508c5652045c392c69
Salario Mínimo <102463669+SalarioMinimo@users.noreply.github.com>
2022-04-19 20:36:37 -0600

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -16,7 +16,7 @@ class calculator:
     self.send = ""
     self.text=word_tokenize(input)
     self.functions = {"más":self.mas, "menos":self.menos, "por":self.por, "sobre":self.sobre, "raiz":self.raiz, "seno":self.trig,
-                      "coseno":self.trig, "tangente":self.trig, "arcoseno":self.trig, "arcocoseno":self.trig, "arcotangente":self.trig
+                      "coseno":self.trig, "tangente":self.trig, "arcoseno":self.trig, "arcocoseno":self.trig, "arcotangente":self.trig,
                      "entre":self.entre}
     self.trigonometric = {"seno":"sin","coseno":"cos","tangente":"tan","arcoseno":"asin","arcocoseno":"acos","arcotangente":"atan"}
     self.references = ("seno","coseno","tangente","arcoseno","arcocoseno","arcotangente","raiz")
