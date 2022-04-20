2537ca2b540600f13594c87e0be9520ee4e57795
Salario Mínimo <102463669+SalarioMinimo@users.noreply.github.com>
2022-04-20 00:21:17 -0600

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -12,7 +12,7 @@ class formatter:
   
   def __init__(self,input):
     self.input=input
-    self.input=self.input.lowercase()
+    self.input=self.input.ascii_lowercase()
     
   def __str__(self):
     return self.input
