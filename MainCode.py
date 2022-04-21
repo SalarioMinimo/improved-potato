4a1473201a39aefe5fbeb6707ca76e4ca58e31ef
Salario Mínimo <102463669+SalarioMinimo@users.noreply.github.com>
2022-04-20 22:01:52 -0600

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -17,11 +17,21 @@ class formatter:
     ordinal = {"cuadrada":"2","cubica":"3","segunda":"2","tercera":"3","cuarta":"4","quinta":"5","sexta":"6","septima":"7","octava":"8",
           "novena":"9","decima":"10"}
     replace = {"á":"a","é":"e","í":"i","ó":"o","ú":"u"}
+    identity = {"uno":"1","dos":"2","tres":"3","cuatro":"4","cinco":"5","seis":"6","siete":"7","ocho":"8","nueve":"9","diez":"10",
+                "once":"11","doce":"12","trece":"13","catorce":"14","quince":"15","dieciseis":"16","diecisiete":"17","dieciocho":"18",
+                "diecinueve":"19","veinte":"20","veintiuno":"21","veintidos":"22","veintitres":"23","veinticuatro":"24",
+                "veinticinco":"25","veintiseis":"26","veintisiete":"27","veintiocho":"28","veintinueve":"29"}
+    identity_dec = {"treinta":"30","cuarenta":"40","cincuenta":"50","sesenta":"60","setenta":"70","ochenta":"80","noventa":"90"}
+    identity_cen = {"ciento":"100","doscientos":"200","trescientos":"300","cuatroscientos":"400","quinientos":"500","seiscientos":"600",
+                    "setecientos":"700","ochocientos":"800","novecientos":"900"}
+    identity_bey = {"mil":"1000","millon":"1000000","millones":"1000000","billones":"1000000000000","trillones":"1000000000000000000"}
     self.text = input.lower()
     for x in replace:
       self.text = self.text.replace(x,replace[x])
     for x in ordinal:
       self.text = self.text.replace(x,ordinal[x])
+    for 
+    
     
     
   def __str__(self):
