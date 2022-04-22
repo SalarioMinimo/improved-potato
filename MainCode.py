a5eea832312213bec8d7ea34cfc25b672e9bc8d2
Salario Mínimo <102463669+SalarioMinimo@users.noreply.github.com>
2022-04-22 03:36:51 -0600

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -174,8 +174,6 @@ class calculator:
     self.text[index+panner-1] = ")"
     
   def conjunto(self,index):
-    if self.text[index+1] == "de":
-      del self.text[index+1]
     self.text[index] = "("
     counter = 1
     panner = 1
