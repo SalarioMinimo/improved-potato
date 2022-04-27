3284fb10e9ee612b535df065fa32f3b55d296712
Salario Mínimo <102463669+SalarioMinimo@users.noreply.github.com>
2022-04-27 14:04:25 -0600

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -11,7 +11,7 @@ class sidebar:
   def __init__(self):
     show = st.sidebar.title("a ver si esto funciona")
     show = st.sidebar.text("esto va a ser la documentación")
-    show = st.sidebar.button(label="xd",on_click=self.calculator)
+    show = st.sidebar.button(label="xd",on_click=self.explanation)
     
   def explanation(self):
     show = st.sidebar.text("este es un nuevo texto.")
