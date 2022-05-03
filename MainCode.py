1f628e1b838e99a9f8e940c1d6ee063d870e41f3
Salario Mínimo <102463669+SalarioMinimo@users.noreply.github.com>
2022-05-03 01:56:33 -0600

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -12,6 +12,7 @@ class str_formatter:
   def __init__(self,input):
     spell = SpellChecker(language=None, case_sensitive=True)
     spell.word_frequency.load_text_file('Dictionary.txt')
+    st.text(spell["seno"])
     
     ordinal = {"cuadrada":"2","cubica":"3","segunda":"2","tercera":"3","cuarta":"4","quinta":"5","sexta":"6","septima":"7","octava":"8",
           "novena":"9","decima":"10"}
