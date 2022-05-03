f6fa697baf44cb5c50657d13aa4920999fbc5696
Salario Mínimo <102463669+SalarioMinimo@users.noreply.github.com>
2022-05-03 00:57:36 -0600

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -163,7 +163,7 @@ class calculator:
 
 
 texto=st.text_input(label="muerte al capital")
-texto = str_formatter(texto)
+texto = str(str_formatter(texto))
 texto
 imprime=str(calculator(texto))
 imprime
