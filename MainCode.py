0c1d9fb3f0e12325b96d77d131f51d4868b83a0d
Salario Mínimo <102463669+SalarioMinimo@users.noreply.github.com>
2022-05-07 05:35:56 -0600

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -90,7 +90,7 @@ class calculator:
                       "coseno":self.trig, "tangente":self.trig, "arcoseno":self.trig, "arcocoseno":self.trig, "arcotangente":self.trig,
                      "entre":self.entre,"conjunto":self.conjunto,"elevado":self.elevado,"fraccion":self.fraccion}
     self.trigonometric = {"seno":"sin","coseno":"cos","tangente":"tan","arcoseno":"asin","arcocoseno":"acos","arcotangente":"atan"}
-    self.references = ("seno","coseno","tangente","arcoseno","arcocoseno","arcotangente","raiz","elevado")
+    self.references = ("seno","coseno","tangente","arcoseno","arcocoseno","arcotangente","raiz","elevado","fraccion")
     for x in range(len(self.text)):
       if self.text[x]==",":
         self.text[x]="."
