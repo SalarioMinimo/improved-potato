c6a3a8b22370f5b451d37526907c96d719581f6e
Salario Mínimo <102463669+SalarioMinimo@users.noreply.github.com>
2022-05-07 15:46:38 -0600

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -246,7 +246,7 @@ class calculator:
    self.text[index+panner] = "/"
    self.text.insert(index+panner,")")
    self.text.insert(index+panner+2,"(")
-   index = index + panner + 2 
+   index = index + panner + 1
    
    counter = 1
    panner=0
